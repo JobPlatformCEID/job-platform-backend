314e90627cf4c8a9ad59b0857aee382824beb3e1
Elias Dimopoulos <linkdevel@protonmail.com>
2026-03-22 17:01:32 +0200

    backend: django/users: EmployerProfile: Add blank=True to company_name
    When a user is created with "POST /api/auth/register/", RegisterSerializer creates
    an empty CandidateProfile or an empty EmployerProfile depending on the role.
    
    This will allow company_name in EmployerProfile to be blank before the user fills
    his EmployerProfile with "PUT /api/employers/me/"

--- users/models.py
+++ users/models.py
@@ -31,7 +31,7 @@ class CandidateProfile(models.Model):
 
 class EmployerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='employer_profile')
-    company_name = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100, blank=True)
     description = models.TextField(blank=True)
     location = models.CharField(max_length=100, blank=True)
     website = models.URLField(blank=True)
