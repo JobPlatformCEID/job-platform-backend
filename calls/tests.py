d4fefd87b62ad1efee2062d3a0d9811ac7001ffc
vortex3964 <theo2005.adams@gmail.com>
2026-03-23 20:49:45 +0200

    backend : added 2 basic tests to test the functions

--- calls/tests.py
+++ calls/tests.py
@@ -1,3 +1,21 @@
+from channels.testing import WebsocketCommunicator
 from django.test import TestCase
+from core.asgi import application
 
-# Create your tests here.
+class WebSocketTest(TestCase):
+    async def test_connect(self):
+        communicator = WebsocketCommunicator(application, "/ws/calls/testroom/")
+        connected, _ = await communicator.connect()
+        self.assertTrue(connected)
+        await communicator.disconnect()
+
+    async def test_send_receive(self):
+        communicator = WebsocketCommunicator(application, "/ws/calls/testroom/")
+        connected, _ = await communicator.connect()
+        self.assertTrue(connected)
+
+        await communicator.send_json_to({"message": "hello"})
+        response = await communicator.receive_json_from()
+        self.assertEqual(response["message"], "hello")
+
+        await communicator.disconnect()
