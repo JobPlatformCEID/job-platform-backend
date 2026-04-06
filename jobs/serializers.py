2d730bef5b59aba21f60670ef7f82800a1da352b
Elias Dimopoulos <linkdevel@protonmail.com>
2026-04-06 22:53:20 +0300

    backend: django/jobs: Add a few more fields to JobApplicationSerializer
    This makes it easier for frontend apps to display a few basic info
    without spamming the server with queries

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -10,6 +10,9 @@ class JobPostingSerializer(serializers.ModelSerializer):
 class JobApplicationSerializer(serializers.ModelSerializer):
     candidate_username = serializers.CharField(source='candidate.user.username', read_only=True)
     candidate_full_name = serializers.SerializerMethodField()
+    job_title = serializers.CharField(source='job.title', read_only=True)
+    company_name = serializers.CharField(source='job.employer.company_name', read_only=True)
+    employer_id = serializers.IntegerField(source='job.employer.id', read_only=True)
 
     class Meta:
         model = JobApplication
