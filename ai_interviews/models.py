623e98bd971c2b8556122875c6dbdf8b11e600b0
vortex3964 <theo2005.adams@gmail.com>
2026-04-10 00:00:05 +0300

    backend : basic model for chats and messages in chats

--- ai_interviews/models.py
+++ ai_interviews/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+# these will be the chats
+class InterviewSession(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE ,related_name='interview_session' )
+    job_role = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f"{self.user} - {self.job_role}"
+
+# these will be the chat messages
+class Message(models.Model):
+    class Role(models.TextChoices):
+        USER = 'user', 'User'
+        ASSISTANT = 'assistant', 'Assistant'
+
+    session = models.ForeignKey(InterviewSession, on_delete=models.CASCADE, related_name='messages')
+    role = models.CharField(max_length=10, choices=Role.choices)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f"{self.role}: {self.content[:50]}"
