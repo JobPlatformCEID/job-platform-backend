62e724bce26d9f7016fdf3a18d75ae77eef4c448
vortex3964 <theo2005.adams@gmail.com>
2026-04-10 03:11:28 +0300

    backend: add session and message API views
    - InterviewSessionListCreateView -> list and create sessions with an
    opening message on creation
    
    - InterviewSessionDetailView -> retrieve or delete a session with
    full message history
    
    - MessageListCreateView -> list messages and create user messages with
    content validation

--- ai_interviews/views.py
+++ ai_interviews/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import InterviewSession, Message
+import logging
+from .serializers import (
+    InterviewSessionSerializer, 
+    InterviewSessionDetailSerializer, 
+    MessageSerializer,
+)
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+class InterviewSessionListCreateView(generics.ListCreateAPIView):
+    serializer_class = InterviewSessionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return InterviewSession.objects.filter(user = self.request.user)
+    
+    def perform_create(self , serializer):
+        session = serializer.save(user=self.request.user)
+        # Generic openning message maybe this will change (but later)
+        opening = 'how may i assist you today'
+        Message.objects.create(session=session , role=Message.Role.Assistant , content = opening)
+
+class InterviewSessionDetailView(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = InterviewSessionDetailSerializer
+
+    def get_queryset(self):
+        return InterviewSession.objects.filter(user=self.request.user)
+
+class MessageListCreateView(generics.ListCreateAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    # find the session were in
+    def get_session(self):
+        return generics.get_object_or_404(
+            InterviewSession,
+            id=self.kwargs['session_id'],
+            user=self.request.user
+        )
+
+    # get all the messages
+    def get_queryset(self):
+        return self.get_session().messages.all()
+
+    def create(self, request, *args, **kwargs):
+        session = self.get_session()
+
+        content = request.data.get('content')
+
+        if not content :
+            logger.error("containt cant be empty")
+            return Response({"error": "content is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Save user message
+        user_msg = Message.objects.create(
+            session=session,
+            role=Message.Role.USER,
+            content=request.data.get('content')
+        )
+
+        return Response(MessageSerializer(user_msg).data, status=status.HTTP_201_CREATED)
+    
