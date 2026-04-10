ca206742f472aa1c6bc93278560b639626711ceb
vortex3964 <theo2005.adams@gmail.com>
2026-04-10 03:14:59 +0300

    backend : added urls

--- ai_interviews/urls.py
+++ ai_interviews/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import InterviewSessionListCreateView, InterviewSessionDetailView, MessageListCreateView
 
 urlpatterns = [
-    
+    path('sessions/', InterviewSessionListCreateView.as_view(), name='session-list-create'),
+    path('sessions/<int:pk>/', InterviewSessionDetailView.as_view(), name='session-detail'),
+    path('sessions/<int:session_id>/messages/', MessageListCreateView.as_view(), name='message-list-create'),
 ]
