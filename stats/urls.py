144c0824bc0a0f8bc4907f5b95a7ba688e36daa7
vortex3964 <theo2005.adams@gmail.com>
2026-04-29 22:54:50 +0300

    backend : add stat to track the 10 most popular skills

--- stats/urls.py
+++ stats/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('jobs-by-title/', views.JobPostingsByTitleView.as_view(), name='jobs-by-title'),
     path('candidates-by-education/', views.CandidatesByEducationLevelView.as_view(), name='candidates-by-education'),
+    path('top-skills/', views.TopSkillsView.as_view(), name='top-skills'),
 ]
--- stats/views.py
+++ stats/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.db.models import Count
 from jobs.models import JobPosting
-from users.models import Education
+from users.models import Education, Skill
 
 class JobPostingsByTitleView(APIView):
     def get(self, request):
@@ -24,4 +24,15 @@ class CandidatesByEducationLevelView(APIView):
             .order_by('-count')
         )
         result = [{'level': item['level'], 'count': item['count']} for item in data]
+        return Response(result)
+
+class TopSkillsView(APIView):
+    def get(self, request):
+        data = (
+            Skill.objects
+            .values('name')
+            .annotate(count=Count('id'))
+            .order_by('-count')[:10]
+        )
+        result = [{'skill': item['name'], 'count': item['count']} for item in data]
         return Response(result)
