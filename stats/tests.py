7f1e3b05514e6edb235a071a520bb9ec36beb7cf
vortex3964 <theo2005.adams@gmail.com>
2026-04-29 22:53:38 +0300

    backend : add test for grouping candidates by their education level

--- stats/tests.py
+++ stats/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient
-from users.models import User, EmployerProfile
+from users.models import User, EmployerProfile , CandidateProfile, Education
 from jobs.models import JobPosting
 
 class JobPostingsByTitleTest(TestCase):
@@ -27,3 +27,24 @@ class JobPostingsByTitleTest(TestCase):
         titles = {item['title']: item['count'] for item in data}
         self.assertEqual(titles['Software Engineer'], 2)
         self.assertEqual(titles['Data Scientist'], 1)
+
+class CandidatesByEducationLevelTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        for i in range(3):
+            user = User.objects.create_user(username=f'candidate{i}', password='pass', role='candidate')
+            profile = CandidateProfile.objects.create(user=user)
+            Education.objects.create(candidate=profile, institution='MIT', degree='CS', level='bachelor')
+
+        user = User.objects.create_user(username='candidate_master', password='pass', role='candidate')
+        profile = CandidateProfile.objects.create(user=user)
+        Education.objects.create(candidate=profile, institution='MIT', degree='CS', level='master')
+
+    def test_returns_grouped_by_education_level(self):
+        url = reverse('candidates-by-education')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        data = {item['level']: item['count'] for item in response.json()}
+        self.assertEqual(data['bachelor'], 3)
+        self.assertEqual(data['master'], 1)
