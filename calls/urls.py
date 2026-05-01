0361623c34ee385bf81d32c7c1c0d63a7602cc6a
vortex3964 <theo2005.adams@gmail.com>
2026-05-01 21:12:21 +0300

    backend : block non participants of calls from seeing them and add endpoints
    added an endpoint to delete and add someone as a participant to the call
    the frontend will handle partcipant management
    
    plus added a filter so that the frontend displays only calls your
    participating in

--- calls/urls.py
+++ calls/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RoomListCreateView, RoomDetailView, RoomTokenView
+from .views import RoomListCreateView, RoomDetailView, RoomTokenView , RoomParticipantView
 
 urlpatterns = [
     path('calls/', RoomListCreateView.as_view()),
     path('calls/<int:pk>/', RoomDetailView.as_view()),
     path('calls/<int:pk>/token/', RoomTokenView.as_view()),
+    path('calls/<int:pk>/participants/', RoomParticipantView.as_view()),
 ]
