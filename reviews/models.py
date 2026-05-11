e2edecb35da07b0447d296b80a2dc224d641f38c
vortex3964 <theo2005.adams@gmail.com>
2026-05-12 02:45:37 +0300

    backend : limit max review score to 5 to match frontend

--- reviews/models.py
+++ reviews/models.py
@@ -6,6 +6,6 @@ class Review(models.Model):
     employer = models.ForeignKey(EmployerProfile, on_delete=models.CASCADE, related_name='reviews')
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='reviews')
     content = models.TextField(null=True, max_length=400, blank=True)
-    score = models.SmallIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(10)])
+    score = models.SmallIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
     created_at = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
